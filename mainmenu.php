7815e489376c408b48e8acf618e12cb23e74fbe9
R Cabot <me@rcabot.com>
2014-11-14 10:20:03 +0000

    Files from gdrive!

---
+++ mainmenu.php
@@ -0,0 +1,15 @@
+<html lang="en">
+  <?php include('./header.php'); ?>
+  <body>
+    <?php include('./topnav.php'); ?>
+
+    <div class="container-fluid">
+      <div class="row">
+        <h1 class="page-header">Welcome, (name).</h1>
+      </div>
+    </div>
+
+    <?php include('./footer.php'); ?>
+    
+  </body>
+</html>
