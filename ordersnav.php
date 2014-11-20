3c6a83c61bca2e6b257f3be481664036f8fbd145
R Cabot <me@rcabot.com>
2014-11-20 16:35:58 +0000

    Added an accordion menu!

--- ordersnav.php
+++ ordersnav.php
@@ -9,9 +9,10 @@ switch ($pageName) {
             <li><a href="./mainmenu.php">Main Menu</a></li>
             <li><a href="./orders.php">Orders Hub</a></li>
             <li><a href="./rules.php">Rules</a></li>
-              <li><a href="./rules.php#"><span class="glyphicon glyphicon-chevron-right"></span> Automation Rules</a></li>
-              <li><a href="./rules.php#"><span class="glyphicon glyphicon-chevron-right"></span> Formatting Rules</a></li>
-            <li><a href="./queue.php">Order Queue</a></li>';
+              <li><a href="#collapseOne" class="collapsed" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="collapseOne"><span class="glyphicon glyphicon-chevron-right"></span> Automation Rules</a></li>
+              <li><a href="#collapseTwo" class="collapsed" data-toggle="collapse" data-parent="#accordion"  aria-expanded="false" aria-controls="collapseTwo"><span class="glyphicon glyphicon-chevron-right"></span> Message Formatting Rules</a></li>
+              <li><a href="#collapseThree" class="collapsed" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="collapseThree"><span class="glyphicon glyphicon-chevron-right"></span> Notification Rules</a></li>
+              <li><a href="./queue.php">Order Queue</a></li>';
         break;
     case "queue.php":
         $extraItems='<div class="col-sm-3 col-md-2 sidebar">
--- rules.php
+++ rules.php
@@ -1,24 +1,119 @@
 <html lang="en">
-  <?php include('./header.php'); ?>
-  <body>
-    <?php include('./topnav.php'); ?>
+	<?php include('./header.php'); ?>
+	<body>
+		<?php include('./topnav.php'); ?>
 
-    <div class="container-fluid">
-      <div class="row">
-        <?php include('./ordersnav.php'); ?>
-        <div class="main col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2">
-          <h1 class="page-header">Message Rules</h1>
-          <div class="well">
-            Here you can edit the rules for:
-            <ul>
-              <li><a href="">Automated Replies</a></li>
-              <li><a href="">Some other rules</a></li>
-              <li><a href="">Etc.</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    <?php include('./footer.php'); ?>
-    
+		<div class="container-fluid">
+			<div class="row">
+				<?php include('./ordersnav.php'); ?>
+				<div class="main col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2">
+					<h1 class="page-header">Message Rules</h1>
+					<div class="well">
+						Here you can edit the rules for:
+						<ul>
+							<li><a href="#collapseOne" class="collapsed" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="collapseOne">Automated Replies</a></li>
+							<li><a href="#collapseTwo" class="collapsed" data-toggle="collapse" data-parent="#accordion"  aria-expanded="false" aria-controls="collapseTwo">Formatting Requirements</a></li>
+							<li><a href="#collapseThree" class="collapsed" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="collapseThree">Notifications</a></li>
+						</ul>
+					</div>
+					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+						<div class="panel panel-default">
+							<div class="panel-heading" role="tab" id="headingOne">
+								<h4 class="panel-title">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+										Automation Rules
+									</a>
+								</h4>
+							</div>
+							<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+								<div class="panel-body">
+									Automatic replies on first and subsequent texts from a number, time periods, etc.
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+								</div>
+							</div>
+						</div>
+						<div class="panel panel-default">
+							<div class="panel-heading" role="tab" id="headingTwo">
+								<h4 class="panel-title">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+										Message Formatting Rules
+									</a>
+								</h4>
+							</div>
+							<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+								<div class="panel-body">
+									Ignore texts if certain phrases aren't included
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+								</div>
+							</div>
+						</div>
+						<div class="panel panel-default">
+							<div class="panel-heading" role="tab" id="headingThree">
+								<h4 class="panel-title">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+										Notification Rules
+									</a>
+								</h4>
+							</div>
+							<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+								<div class="panel-body">
+									Notify for certain phrases sent in texts
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+									<br/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php include('./footer.php'); ?>
+		
 </body></html>
