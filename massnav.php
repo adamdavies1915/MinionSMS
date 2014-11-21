a47c800d70d9597c5183288c87375d5c06c06ee8
rhothi <riley.hothi@googlemail.com>
2014-11-21 07:31:10 +0000

    Create massnav.php

---
+++ massnav.php
@@ -0,0 +1,36 @@
+<?php
+$pageName = basename($_SERVER['PHP_SELF']);
+$pageEnd = '</ul>
+        </div><!-- end #navbarexample -->';
+switch ($pageName) {
+    case "rules.php":
+        $extraItems='<div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li><a href="./mainmenu.php">Main Menu</a></li>
+            <li><a href="./masscontacts.php">View contacts</a></li>
+            <li><a href="./newcontact.php">Add new contact</a></li>
+              <li><a href="#collapseOne" class="collapsed" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="collapseOne"><span class="glyphicon glyphicon-chevron-right"></span> Automation Rules</a></li>
+              <li><a href="#collapseTwo" class="collapsed" data-toggle="collapse" data-parent="#accordion"  aria-expanded="false" aria-controls="collapseTwo"><span class="glyphicon glyphicon-chevron-right"></span> Message Formatting Rules</a></li>
+              <li><a href="#collapseThree" class="collapsed" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="collapseThree"><span class="glyphicon glyphicon-chevron-right"></span> Notification Rules</a></li>
+              <li><a href="./sendmass.php">Send mass message</a></li>';
+        break;
+    case "queue.php":
+        $extraItems='<div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li><a href="./mainmenu.php">Main Menu</a></li>
+            <li><a href="./masscontacts.php">View contacts</a></li>
+            <li><a href="./newcontact.php">Add new contact</a></li>
+            <li><a href="./sendmass.php">Send mass message</a></li>';
+        break;
+    default:
+        $extraItems='<div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li><a href="./mainmenu.php">Main Menu</a></li>
+            <li><a href="./masscontacts.php">View contacts</a></li>
+            <li><a href="./newcontact.php">Add new contact</a></li>
+            <li><a href="./sendmass.php">Send mass message</a></li>';
+        break;
+}
+echo $extraItems,$pageEnd;
+
+?>
