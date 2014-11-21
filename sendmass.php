349988635af85c24489a906239c70aae5284a5c6
rhothi <riley.hothi@googlemail.com>
2014-11-21 07:33:13 +0000

    Create sendmass.php

---
+++ sendmass.php
@@ -0,0 +1,34 @@
+<html lang="en">
+  <?php include('./header.php'); ?>
+  <body>
+    <?php include('./topnav.php'); ?>
+
+    <div class="container-fluid">
+      <div class="row">
+        <?php include('./masssnav.php'); ?>
+        <div class="main col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2">
+          <h1 class="page-header">Send a mass message</h1>
+          <div class="well">
+            Please select a contact group and enter a message
+			<br><br>
+			<select><option value="Undefined">Undefined</option><option value="Employees">Employees</option><option value="Customers">Customers</option></select>
+            
+			<form method="post" action="">
+				<textarea name="comments" cols="25" rows="5">
+				Enter message here...
+				</textarea><br>
+				<input type="submit" value="Send" />
+			</form>
+			
+			
+			<ul>
+              <li><a href="./mass.php">Go back</a></li>
+            </ul>
+          </div>
+          <?php include('./latest.php'); ?>
+        </div>
+      </div>
+    </div>
+    <?php include('./footer.php'); ?>
+    
+</body></html>
