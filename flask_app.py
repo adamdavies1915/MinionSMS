3d34425e6369264ad8a78d2b3ac23931032e314b
Adam Davies <adamdavies1915@gmail.com>
2014-12-05 10:40:09 +0000

    testing out twilio stuff

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,14 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, request, redirect
+import twilio.twiml
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+@app.route("/", methods=['GET', 'POST'])
+def sendResponse():
+    print "message received"
+    response = twilio.twiml.Response()
+    response.message("Auto-response")
+    return str(response)
 
+if __name__ == "__main__":
+    app.run(debug=True)
