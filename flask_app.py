d4145fd0733f791611af7e6a7938770082500da6
Adam Davies <adamdavies1915@gmail.com>
2015-02-04 14:27:53 +0000

    WIP handle a post request to send a sms

--- flask_app.py
+++ flask_app.py
@@ -10,5 +10,11 @@ def sendResponse():
     response.message("Auto-response")
     return str(response)
 
+@app.route("/webapi/masssms", methods=['POST'])
+def sendSms():
+	if not request.json: # or not "title" in request.json:
+		abort(400)
+	print request.json
+
 if __name__ == "__main__":
     app.run(debug=True)
