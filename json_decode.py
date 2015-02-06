d6f9f00e9f74e2fba64a302b10b6676fd64c7030
Tom Henderson <thomashthomas@gmail.com>
2015-02-06 14:27:32 +0000

    Json Checking
    Seems to work, need to figure out we will let the user set their own
    rules

--- json_decode.py
+++ json_decode.py
@@ -1,8 +1,21 @@
 import json
+import time
 
 def checkjson(a):
 	b = json.loads(a)
 	if len(b["number"]) == 11:
 		if len(b["message"]) !=0:
-			return true
-	return false
+			print 'true'
+			return True
+	print 'false'
+	return False
+
+
+jsonTest = {'message' : 'Hello, World!', 'number' : '07947393586'}
+checkjson(json.dumps(jsonTest))
+jsonTest = {'message' : 'Hello, World!', 'number' : '079473'}
+checkjson(json.dumps(jsonTest))
+jsonTest = { 'number' : '079473'}
+checkjson(json.dumps(jsonTest))
+
+time.sleep(5)
