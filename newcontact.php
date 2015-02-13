8f2ea854c53340adebe9b8a7de244c869b5eaa80
R Cabot <me@rcabot.com>
2015-02-13 15:11:49 +0000

    Can create contacts now!

--- newcontact.php
+++ newcontact.php
@@ -8,32 +8,44 @@
         <?php include('./massnav.php'); ?>
         <div class="main col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2">
           <h1 class="page-header">Edit/Add new contact</h1>
-          <div class="well">
+          <div class="well" id="contactForm">
             Please fill in the following details
 			<p></p>
 			
 			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Forename">
+				<input id = "firstNText" type="text" class="form-control" placeholder="Forename">
 			</div>
 			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Surname">
+				<input id = "surNText" type="text" class="form-control" placeholder="Surname">
 			</div>
 			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Phone number">
+				<input id = "numberText" type="text" class="form-control" placeholder="Phone number">
 			</div>
 			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Contact Group">
+				<input id = "groupText" type="text" class="form-control" placeholder="Contact Group">
 			</div>
 			<br>
 			
-			<a href=""><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-save" aria-hidden="true"></span> Save</button></a>
+			<button id="create" type="button" class="btn btn-default"><span class="glyphicon glyphicon-save" aria-hidden="true"></span> Save</button></a>
 			<br>
-			<a href="./mass.php"><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Back without saving</button></a>
+			<a href="./mass.php"><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Back without saving</button>
 			
           </div>
         </div>
       </div>
     </div>
     <?php include('./footer.php'); ?>
-    
+    <script>
+	    function transmit(){
+	    	//document.getElementById("contactForm").disabled = true;
+	    	var myFirebaseRef = new Firebase("https://group15.firebaseio.com/contact");
+	    	myFirebaseRef.push({
+	    		name : document.getElementById("firstNText").value+" "+document.getElementById("surNText").value,
+			    number: document.getElementById("numberText").value,
+			    groupid: document.getElementById("groupText").value
+			});
+
+		}
+		document.getElementById("create").addEventListener("click", transmit, false);
+    </script>
 </body></html>
