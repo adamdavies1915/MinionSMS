1953455c247d17869ac9461c432e2cd6f07b4e21
Adam Davies <adamdavies1915@gmail.com>
2015-03-03 15:04:17 +0000

    WIP can get a list of contacts from the group

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, redirect, jsonify
 import twilio.twiml
+from firebase import firebase
+import json
 
 app = Flask(__name__)
 
@@ -12,15 +14,24 @@ def sendResponse():
 
 @app.route('/hello', methods=['GET'])
 def get_tasks():
-    return "hello"
+    database = firebase.FirebaseApplication("https://group15.firebaseio.com", None)
+
+    result = database.post("/hello", "hello world")
+    return "ok"
 
 @app.route("/webapi/masssms", methods=['POST'])
 def sendSms():
 	# if not request.json: # or not "title" in request.json:
 	# 	abort(400) #currently will accept any post request
-	print "ok"
-	print request.json
-	return jsonify({'number': 2}), 201
+	# return request.json.get("group")
+    database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
+    result = database.get("group/", request.json.get("group"))
+    # return request.json.get("group")
+    input = json.dumps(result["contacts"])
+    return str(input)
+    # return "ok"
+
+	# return jsonify({'number': 2}), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
