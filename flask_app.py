d9c842c2f2e97c4fc516d2d82214568a9b6bc248
Adam Davies <adamdavies1915@gmail.com>
2015-03-06 14:57:28 +0000

    WIP can now send group sms. Needs error handeling

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, redirect, jsonify
 import twilio.twiml
+from twilio.rest import TwilioRestClient
 from firebase import firebase
 import json
 
 app = Flask(__name__)
+
+account_sid = "AC6db8f3d3a7c1d160f663c5e615bbaade"
+auth_token = "8a6ec3b89f099e9d437089d1bd50cac6"
+client = TwilioRestClient(account_sid, auth_token)
 
 @app.route("/", methods=['GET', 'POST'])
 def sendResponse():
@@ -22,16 +27,22 @@ def get_tasks():
 @app.route("/webapi/masssms", methods=['POST'])
 def sendSms():
 	# if not request.json: # or not "title" in request.json:
-	# 	abort(400) #currently will accept any post request
+	# abort(400) #currently will accept any post request
 	# return request.json.get("group")
     database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
-    result = database.get("group/", request.json.get("group"))
-    # return request.json.get("group")
-    input = json.dumps(result["contacts"])
-    return str(input)
-    # return "ok"
+    result = database.get("group/", request.json.get("group")+"/contacts")
+    if result == None:
+    	return "Error"
+    for x in result:
+    	# try:
+    	if x != "":
+	    	number = database.get("contact/", result[x]["contactid"])["number"]
+	    	message = client.messages.create(to=number, from_="+441255411083",
+	                                     body=request.json.get("message"))
 
-	# return jsonify({'number': 2}), 201
+	  	# except twilio.TwilioRestException: eror handeling would be a very good idea
+	   #  	print e
+    return "ok"
 
 if __name__ == "__main__":
     app.run(debug=True)
