56089423c67efcd7ea9939198ae016e61a484cb5
Adam Davies <adamdavies1915@gmail.com>
2015-03-10 15:01:59 +0000

    forced update

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, redirect, jsonify
-import twilio.twiml, twilio.rest
-#import json
+import twilio.twiml
+from twilio.rest import TwilioRestClient
 from firebase import firebase
+import json
 from flask.ext.cors import CORS, cross_origin
 
 
@@ -9,55 +10,51 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+account_sid = "AC6db8f3d3a7c1d160f663c5e615bbaade"
+auth_token = "8a6ec3b89f099e9d437089d1bd50cac6"
+client = TwilioRestClient(account_sid, auth_token)
 
 @app.route("/", methods=['GET', 'POST'])
+def sendResponse():
+    print "message received"
+    response = twilio.twiml.Response()
+    response.message("Auto-response")
+    return str(response)
 
 
 @app.route('/twilioapi', methods=['GET'])
 def incoming_message():
-    account_sid = "AC6db8f3d3a7c1d160f663c5e615bbaade"
-    auth_token  = "8a6ec3b89f099e9d437089d1bd50cac6"
-
-    client = twilio.rest.TwilioRestClient(account_sid, auth_token)
 
     #get details of incoming message
-    sender = request.values.get('From', None)
+    senderNumber = request.values.get('From', None)
     messageBody = request.values.get('Body', None)
-    dealtWith = False
-
-    database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
 
     #then log the message
-    messageData = {'dealtwith' : dealtWith, 'message' : messageBody, 'number' : sender}
-    database.post('/messages', messageData)
-
-    if 'order' in str(messageBody).lower():
-        #process_order()
-        newMessageBody = "Thank you for your order. Your order number is h9843ru4hfu"
-        try:
-            client.messages.create(body=newMessageBody,
-                                             to = sender,
-                                             from_="+441255411083")
-
-            replyData = {'groupID' : 'group', 'message' : newMessageBody}
-            database.post('/outgoing', replyData)
-
-        except twilio.TwilioRestException as e:
-            print e
-
-    return 'ok'
-
-#def process_order(sender, order, db, twilioClient):
-
+    database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
+    messageData = {'message' : messageBody, 'number' : senderNumber}
+    result = database.post('/messages', messageData)
+    return "ok"
 
 @app.route("/webapi/masssms", methods=['POST'])
 @cross_origin()
 def sendSms():
 	# if not request.json: # or not "title" in request.json:
-	# 	abort(400) #currently will accept any post request
-    # print request.json
-	return "ok"
-# 	return jsonify({'number': 2}), 201
+	# abort(400) #currently will accept any post request
+	# return request.json.get("group")
+    database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
+    result = database.get("group/", request.json.get("group")+"/contacts")
+    if result == None:
+    	return "Error"
+    for x in result:
+    	# try:
+    	if x != "":
+	    	number = database.get("contact/", result[x]["contactid"])["number"]
+	    	message = client.messages.create(to=number, from_="+441255411083",
+	                                     body=request.json.get("message"))
+
+	  	# except twilio.TwilioRestException: eror handeling would be a very good idea
+	   #  	print e
+    return "ok"
 
 if __name__ == "__main__":
     app.run(debug=True)
