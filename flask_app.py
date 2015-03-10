22426bbf2d9eefdad7ccb0be1bc327ab3626644a
Adam Davies <adamdavies1915@gmail.com>
2015-03-10 14:20:47 +0000

    changes on python anywhere

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, jsonify
-import twilio.twiml
+import twilio.twiml, twilio.rest
 #import json
 from firebase import firebase
 from flask.ext.cors import CORS, cross_origin
@@ -15,16 +15,39 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/twilioapi', methods=['GET'])
 def incoming_message():
+    account_sid = "AC6db8f3d3a7c1d160f663c5e615bbaade"
+    auth_token  = "8a6ec3b89f099e9d437089d1bd50cac6"
+
+    client = twilio.rest.TwilioRestClient(account_sid, auth_token)
 
     #get details of incoming message
-    senderNumber = request.values.get('From', None)
+    sender = request.values.get('From', None)
     messageBody = request.values.get('Body', None)
 
-    #then log the message
     database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
-    messageData = {'message' : messageBody, 'number' : senderNumber}
-    result = database.post('/messages', messageData)
-    return "ok"
+
+    #then log the message
+    messageData = {'message' : messageBody, 'number' : sender}
+    database.post('/messages', messageData)
+
+    if 'order' in str(messageBody).lower():
+        #process_order()
+        newMessageBody = "Thank you for your order. Your order number is h9843ru4hfu"
+        try:
+            client.messages.create(body=newMessageBody,
+                                             to = sender,
+                                             from_="+441255411083")
+
+            replyData = {'groupID' : 'group', 'message' : newMessageBody}
+            database.post('/outgoing', replyData)
+
+        except twilio.TwilioRestException as e:
+            print e
+
+    return 'ok'
+
+#def process_order(sender, order, db, twilioClient):
+
 
 @app.route("/webapi/masssms", methods=['POST'])
 @cross_origin()
