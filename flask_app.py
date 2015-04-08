079fd54a03054cd49ef0bbc5162c8667a575b88a
Adam Davies <adamdavies1915@gmail.com>
2015-04-08 13:35:53 +0000

    small commit added comment

--- flask_app.py
+++ flask_app.py
@@ -5,6 +5,7 @@ from firebase import firebase
 import json
 from flask.ext.cors import CORS, cross_origin
 
+# From pythonanywhere support
 import os
 from urlparse import urlparse
 
@@ -15,6 +16,7 @@ proxy_url = os.environ.get("http_proxy")
 host, port = urlparse(proxy_url).netloc.split(":")
 Connection.set_proxy_info(host, int(port), proxy_type=PROXY_TYPE_HTTP)
 
+#end
 
 app = Flask(__name__)
 cors = CORS(app)
