a331d71e531824745cca95a60d2fc11041514050
Adam Davies <adamdavies1915@gmail.com>
2015-04-11 12:19:50 +0100

    working api for ordering

--- flask_app.py
+++ flask_app.py
@@ -6,15 +6,15 @@ import json
 from flask.ext.cors import CORS, cross_origin
 
 # From pythonanywhere support- only use in python anywhere NOT localy
-import os
-from urlparse import urlparse
+# import os
+# from urlparse import urlparse
 
-from twilio.rest.resources import Connection
-from twilio.rest.resources.connection import PROXY_TYPE_HTTP
+# from twilio.rest.resources import Connection
+# from twilio.rest.resources.connection import PROXY_TYPE_HTTP
 
-proxy_url = os.environ.get("http_proxy")
-host, port = urlparse(proxy_url).netloc.split(":")
-Connection.set_proxy_info(host, int(port), proxy_type=PROXY_TYPE_HTTP)
+# proxy_url = os.environ.get("http_proxy")
+# host, port = urlparse(proxy_url).netloc.split(":")
+# Connection.set_proxy_info(host, int(port), proxy_type=PROXY_TYPE_HTTP)
 
 #end
 
@@ -33,7 +33,6 @@ def sendResponse():
     response.message("Auto-response")
     return str(response)
 
-
 @app.route('/twilioapi', methods=['GET', 'POST'])
 def incoming_message():
 
@@ -44,23 +43,18 @@ def incoming_message():
 
     database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
 
-    #then log the message
-    messageData = {'dealtwith' : dealtWith, 'message' : messageBody, 'number' : sender}
-    database.post('/messages', messageData)
-
     replyData = "OK" #change
     response = twilio.twiml.Response()
 
-
     if 'order' in str(messageBody).lower():
-        #process_order()
-        response.say("Thank you for your order. Your order number is h9843ru4hfu")
         orderBody = request.values.get("Body", None)
         fromNumber = request.values.get("From", None)
-        orderData = {'orderBody': orderBody, 'fromNumber': fromNumber}
-        database.post('/orders', orderData)
-
-        # database.post('/outgoing', replyData)
+        orderID = database.post('/messages', None).get('name') #get id from firebase first so that ID 
+                                                               #so that ID can be in the object. Needed 
+                                                               #for order completion ect.
+        orderData = {'dealtwith': False, 'message': orderBody, 'number': fromNumber, 'orderID': orderID}
+        database.patch('/messages/'+orderID, orderData)
+        response.say("Thank you for your order. Your order is currently being processed and will be accepted shortly. Your order id is "+orderID)
 
     ## Otherwise Tree Responses
 
@@ -70,30 +64,67 @@ def incoming_message():
 @app.route("/webapi/masssms", methods=['POST'])
 @cross_origin()
 def sendSms():
-	# if not request.json: # or not "title" in request.json:
-	# abort(400) #currently will accept any post request
-	# return request.json.get("group")
+    # if not request.json: # or not "title" in request.json:
+    # abort(400) #currently will accept any post request
+    # return request.json.get("group")
     database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
     result = database.get("group/", request.json.get("group")+"/contacts")
     if result == None:
-    	return "Error"
+        return "Error"
     for x in result:
-    	# try:
-    	if x != "":
-	    	number = database.get("contact/", result[x]["contactid"])["number"]
-	    	message = client.messages.create(to=number, from_="+441255411083",
-	                                     body=request.json.get("message"))
+        # try:
+        if x != "":
+            number = database.get("contact/", result[x]["contactid"])["number"]
+            message = client.messages.create(to=number, from_="+441255411083",
+                                         body=request.json.get("message"))
 
-	  	# except twilio.TwilioRestException: eror handeling would be a very good idea
-	   #  	print e
+        # except twilio.TwilioRestException: eror handeling would be a very good idea
+       #    print e
     return "200 OK"
 
-@app.route("/webapi/orderComplete", methods=['POST'])
+@app.route("/webapi/ordercomplete", methods=['POST'])
 @cross_origin()
 def completeOrder():
-    database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
-    request.json.get("orderID")
 
+    database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
+    orderID = request.json.get("orderID")
+
+    number = database.get("messages/"+orderID+"/number", None)
+    message = "Your order is now ready." 
+    client.messages.create(to=number, from_="+441255411083",
+                                         body=message)
+
+
+
+    database.patch('/messages/'+orderID, {'dealtwith': True})
+    return  "200 OK"
+
+
+@app.route("/webapi/orderaccepted", methods=['POST'])
+@cross_origin()
+def acceptOrder():
+    database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
+    orderID = request.json.get("orderID")
+
+    number = database.get("messages/"+orderID+"/number", None)
+    message = "Your order has been accepted" 
+    client.messages.create(to=number, from_="+441255411083",
+                                         body=message)
+    return "200 OK"
+
+@app.route("/webapi/orderdeclined", methods=['POST'])
+@cross_origin()
+def declineOrder():
+    database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
+    orderID = request.json.get("orderID")
+    
+    number = database.get("messages/"+orderID+"/number", None)
+    message = "Sorry you're order has been declined. Please contact us directly" 
+    client.messages.create(to=number, from_="+441255411083",
+                                         body=message)
+
+    database.delete('/messages/'+orderID, None)
+    return "200 OK"
 
 if __name__ == "__main__":
     app.run(debug=True)
