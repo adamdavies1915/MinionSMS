a54841bb568dd8f40fe2eae03939fd3715ca46f4
Adam Davies <adamdavies1915@gmail.com>
2015-04-29 09:20:08 +0000

    added Adam B's auto rules and Tom's config

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,8 @@ from twilio.rest import TwilioRestClient
 from firebase import firebase
 import json
 from flask.ext.cors import CORS, cross_origin
+import ConfigParser
+import time
 
 # From pythonanywhere support- only use in python anywhere NOT localy
 import os
@@ -22,9 +24,21 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-account_sid = "AC6db8f3d3a7c1d160f663c5e615bbaade"
-auth_token = "8a6ec3b89f099e9d437089d1bd50cac6"
+account_sid = "AC301918810de783fef63162d58afd5052"
+auth_token = "c96ec8bfd7c5d5abcb48f253ce3cb25b"
 client = TwilioRestClient(account_sid, auth_token)
+
+Config = ConfigParser.RawConfigParser()
+Config.read('mysite/config.cfg')
+twilioNumber = Config.get('data', 'number')
+print twilioNumber
+twilioNumber = +441173252277
+print twilioNumber
+newOrderMessage = Config.get('data', 'new')
+declinedMessage = Config.get('data', 'declined')
+acceptedMessage = Config.get('data', 'accepted')
+readyMessage = Config.get('data', 'ready')
+
 
 @app.route("/", methods=['GET', 'POST'])
 def sendResponse():
@@ -35,31 +49,61 @@ def sendResponse():
 
 @app.route('/twilioapi', methods=['GET', 'POST'])
 def incoming_message():
+    database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
 
     #get details of incoming message
     sender = request.values.get('From', None)
     messageBody = request.values.get('Body', None)
+    contactsList = database.get('/contact', None)
     dealtWith = False
 
-    database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
 
-    replyData = "OK" #change
+    replyData = "500" #server error if something breaks
     response = twilio.twiml.Response()
 
     if 'order' in str(messageBody).lower():
         orderBody = request.values.get("Body", None)
         fromNumber = request.values.get("From", None)
-        orderID = database.post('/messages', None).get('name') #get id from firebase first so that ID 
-                                                               #so that ID can be in the object. Needed 
+        orderID = database.post('/messages', None).get('name') #get id from firebase first so that ID
+                                                               #so that ID can be in the object. Needed
                                                                #for order completion ect.
         orderData = {'dealtwith': False, 'message': orderBody, 'number': fromNumber, 'orderID': orderID}
         database.patch('/messages/'+orderID, orderData)
-        response.message("Thank you for your order. Your order is currently being processed and will be accepted shortly. Your order id is "+orderID)
+        response.message(newOrderMessage + " "+orderID)
 
-    ## Otherwise Tree Responses
+     #if not an order use auto rules
+    else:
+        rules = database.get('/rules/information/ifNewUser/rules', None)
+
+        if messageBody in rules:
+            pathway = '/rules/information/ifNewUser/rules/' + messageBody
+            autoResponse = database.get('/rules/information/ifNewUser/rules/' + messageBody + '/reply', None)
+            client.messages.create(to= sender, from_=twilioNumber, body= autoResponse)
+            if 'subrules' in database.get(pathway, None):
+                database.post('/autoStates', {'number' : sender, 'lastmessage' : messageBody, 'pathway' : pathway})
+        else:
+            autoUsers = database.get('/autoStates', None)
+
+            for user in autoUsers:
+                if sender == database.get('/autoStates/' + user + '/number', None):
+                    subRules(database.get('/autoStates/' + user + '/pathway', None), sender, messageBody, database, user)
 
 
     return str(response)
+
+def subRules(pathway, number, lastMsg, db, autoID):
+
+    if 'subrules' in db.get(pathway, None):
+        validResponses = db.get(pathway +'/subrules' , None)
+
+        if lastMsg in validResponses:
+            response = db.get(pathway + '/subrules/' + lastMsg + '/reply', None)
+            client.messages.create(to= number, from_=twilioNumber, body= response)
+            newPath = pathway + '/subrules/' + lastMsg
+            if 'subrules' in db.get(newPath, None):
+                db.patch('/autoStates/' + autoID, {'lastmessage' : lastMsg, 'number' : number, 'pathway' : newPath})
+            else:
+                db.delete('/autoStates/', autoID)
 
 @app.route("/webapi/masssms", methods=['POST'])
 @cross_origin()
@@ -71,9 +115,9 @@ def sendSms():
     for x in result:
         if x != "":
             number = database.get("contact/", result[x]["contactid"])["number"]
-            message = client.messages.create(to=number, from_="+441255411083",
+            message = client.messages.create(to=number, from_=twilioNumber,
                                          body=request.json.get("message"))
-          
+
     return "200 OK"
 
 @app.route("/webapi/orderready", methods=['POST'])
@@ -84,8 +128,8 @@ def completeOrder():
     orderID = request.json.get("orderID")
 
     number = database.get("messages/"+orderID+"/number", None)
-    message = "Your order is now ready." 
-    client.messages.create(to=number, from_="+441255411083",
+    message = readyMessage
+    client.messages.create(to=number, from_=twilioNumber,
                                         body=message)
 
     database.patch('/messages/'+orderID, {'status': 'ready'})
@@ -99,8 +143,8 @@ def acceptOrder():
     orderID = request.json.get("orderID")
 
     number = database.get("messages/"+orderID+"/number", None)
-    message = "Your order has been accepted" 
-    client.messages.create(to=number, from_="+441255411083",
+    message = acceptedMessage
+    client.messages.create(to=number, from_=twilioNumber,
                                          body=message)
     database.patch('/messages/'+orderID, {'status': 'accepted'})
     return "200 OK"
@@ -110,10 +154,10 @@ def acceptOrder():
 def declineOrder():
     database = firebase.FirebaseApplication('https://group15.firebaseio.com/', None)
     orderID = request.json.get("orderID")
-    
+
     number = database.get("messages/"+orderID+"/number", None)
-    message = "Sorry you're order has been declined. Please contact us directly" 
-    client.messages.create(to=number, from_="+441255411083",
+    message = declinedMessage
+    client.messages.create(to=number, from_=twilioNumber,
                                          body=message)
 
     database.delete('/messages/'+orderID, None)
